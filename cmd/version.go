6af232a1737089283a046a3225e6435c484d0b3c
Da Chen <da.chen@me.com>
2026-05-16 14:53:39 -0700

    Add version cmd

---
+++ cmd/version.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2026 Da Chen <da01.chen@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "print version info",
+	Long:  `print version info`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.1 - initial project")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
